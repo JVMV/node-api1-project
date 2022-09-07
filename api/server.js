6f4775fe595e128f45d2648d011ad859e8242c7b
Justin Abellera <justin.c.abellera@gmail.com>
2022-09-07 15:47:28 -0700

    server delete finished

--- api/server.js
+++ api/server.js
@@ -6,12 +6,55 @@ const server = express()
 
 server.use(express.json())
 
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        if(!name || !bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            const newUser = await User.insert({name, bio})
+            res.status(201).json(newUser)
+        }
+    } catch(err) {
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
+
 server.get('/api/users', async (req, res) => {
     try {
         const users = await User.find()
         res.status(200).json(users)
     } catch(err) {
         res.status(500).json({message: `Something happened ${err.message}`})
+    }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+        if(!user) {
+            res.status(404).json({ message: '/does not exist/' })
+        } else {
+            res.status(200).json(user)
+        }
+        
+    } catch(err) {
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedUser = await User.remove(id)
+        if(!deletedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.status(200).json(deletedUser)
+        }
+    } catch(err) {
+        res.status(500).json({ message: "The user could not be removed" })
     }
 })
 
