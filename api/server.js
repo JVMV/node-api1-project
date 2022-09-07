cdf2b60b69a1098d2ab4807d29b43d8b23995713
Justin Abellera <justin.c.abellera@gmail.com>
2022-09-07 14:46:50 -0700

    server created

--- api/server.js
+++ api/server.js
@@ -1,6 +1,35 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
+const User = require('./users/model')
 
 const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch(err) {
+        res.status(500).json({message: `Something happened ${err.message}`})
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
